9212fcfa7c8ea6b6824168105c64ebd70cbfac53
tahashieenavaz <tahashieenavaz@gmail.com>
2025-02-26 13:39:48 +0100

    nah

--- hoverbee.js
+++ hoverbee.js
@@ -13,6 +13,9 @@ function hoverbee({
   threshold = 200,
   duration = 300,
   fallbackDuration = 300,
+  triggerEvent = null,
+  x = true,
+  y = true,
 }) {
   let elementInstance = null;
   let calculatedStrength = strength / 100;
@@ -29,30 +32,33 @@ function hoverbee({
 
   const box = elementInstance.getBoundingClientRect();
 
-  elementInstance.addEventListener("mouseenter", () => {
-    let callback = (e) => {
-      const centerX = (box.left + box.right) / 2;
-      const centerY = (box.top + box.bottom) / 2;
-      const moveX = centerX - e.x;
-      const moveY = centerY - e.y;
-      const factor = 1 / calculatedStrength;
-      Hoverbee.animate(
-        elementInstance,
-        -moveX / factor,
-        -moveY / factor,
-        duration
-      );
+  let callback = (e) => {
+    const centerX = (box.left + box.right) / 2;
+    const centerY = (box.top + box.bottom) / 2;
+    const moveX = centerX - e.x;
+    const moveY = centerY - e.y;
+    const factor = 1 / calculatedStrength;
+    Hoverbee.animate(
+      elementInstance,
+      x ? -moveX / factor : 0,
+      y ? -moveY / factor : 0,
+      duration
+    );
+    const distance = Math.sqrt(
+      Math.pow(e.x - centerX, 2) + Math.pow(centerY - e.y, 2)
+    );
 
-      const distance = Math.sqrt(
-        Math.pow(e.x - centerX, 2) + Math.pow(centerY - e.y, 2)
-      );
+    if (distance > threshold) {
+      removeEventListener("mousemove", callback);
+      Hoverbee.animate(elementInstance, 0, 0, fallbackDuration);
+    }
+  };
 
-      if (distance > threshold) {
-        removeEventListener("mousemove", callback);
-        Hoverbee.animate(elementInstance, 0, 0, fallbackDuration);
-      }
-    };
-
+  if (!triggerEvent) {
     addEventListener("mousemove", callback);
-  });
+  } else {
+    elementInstance.addEventListener(triggerEvent, () => {
+      addEventListener("mousemove", callback);
+    });
+  }
 }
